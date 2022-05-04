bf2e581d251f99666714fbbb460173020fd7b619
Benny Neugebauer <mail@bennycode.com>
2022-05-04 02:07:25 +0200

    Set empty config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@
 const filter = require('./filter');
 
 const config = hexo.config['hexo-insert-toc'] || {};
-hexo.config['hexo-insert-toc'] = Object.freeze(config);
+hexo.config['hexo-insert-toc'] = config;
 
 hexo.extend.filter.register('before_post_render', filter.insertToc);
