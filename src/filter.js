d3a8de05a072e0ff3ecbf629d7fb0970ad049c59
Benny Neugebauer <mail@bennycode.com>
2022-05-04 02:06:42 +0200

    Fix import

--- src/filter.js
+++ src/filter.js
@@ -1,4 +1,5 @@
 const toc = require('markdown-toc');
+const slugify = require('./slugify');
 
 exports.insertToc = function (data) {
   data.content = toc.insert(data.content, {...this.config['hexo-insert-toc'], slugify});
